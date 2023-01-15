c541cd807468a205e6d8de7558589f9738da5d63
yuI4140 <102521443+yuI4140@users.noreply.github.com>
2023-01-15 19:40:57 +0100

    Update main.rs

--- Search/src/main.rs
+++ Search/src/main.rs
@@ -1,5 +1,5 @@
 // #![allow(unused)] // For beginning only.
-
+// Made by YuI4140
 use crate::prelude::*;
 use std::fs::read_dir;
 
@@ -9,11 +9,5 @@ mod utils;
 
 fn main() -> Result<()> {
 	println!("Hello, world!");
-
-	for entry in read_dir("./")?.filter_map(|e| e.ok()) {
-		let entry: String = W(&entry).try_into()?;
-		println!("{entry}");
-	}
-
-	Ok(())
+Ok(())
 }
